03b39f9c449fd8c73c3bf31e4e5c547586f08ec6
Queen-02 <queenzyblack176@gmail.com>
2022-04-08 07:25:13 +0300

    created main file

---
+++ src/main.kt
@@ -0,0 +1,22 @@
+fun main(){
+    //question one
+    oddNumbers()
+
+}
+//a function that prints out all the odd numbers between 1 and 200
+fun oddNumbers(){
+    for (number in 1..20){
+        if (number%2!=0){
+            println(number)
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
